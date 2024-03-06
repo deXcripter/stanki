a7e8978a63ceafcb21d194d4178e8ffba1cd4301
johnpaul <indisputable.jay@gmail.com>
2024-03-06 13:36:57 +0100

    coding

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,13 @@
 import express from 'express';
 import globalErrorHandler from './controllers/globalErrorHandler';
 import morgan from 'morgan';
+import versionOne from './v1';
 
 // app
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use('api/v1');
+app.use('api/v1', versionOne);
 app.use(globalErrorHandler);
 
 // exporting app
