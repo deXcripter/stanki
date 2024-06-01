47c533ee040c003db5072a3f81422b11be9cfb8d
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-06-01 20:20:31 +0100

    settign up my envireoment

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import http from "http";
+
+dotenv.config({ path: path.join(__dirname, "../config.env") });
+
+console.log("##### Connecting to the Database #####");
+mongoose
+  .connect(process.env.LOCAL_DB!)
+  .then(() => {
+    const server = http.createServer((req, res) => {});
+    server.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is currently running on port ${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("An error occured when connecting to the database");
+    process.exit(1);
+  });
