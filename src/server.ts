431cf5285de19bec0d28f14773632ceccd5a5366
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-06-02 12:26:53 +0100

    prettierrc

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import path from "path";
-import http from "http";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import http from 'http';
 
-dotenv.config({ path: path.join(__dirname, "../config.env") });
+dotenv.config({ path: path.join(__dirname, '../config.env') });
 
-console.log("##### Connecting to the Database #####");
+console.log('##### Connecting to the Database #####');
 mongoose
   .connect(process.env.LOCAL_DB!)
   .then(() => {
     const server = http.createServer((req, res) => {});
     server.listen(process.env.PORT || 8000, () => {
       console.log(
-        `Server is currently running on port ${process.env.PORT || 8000}`
+        `Server is currently running on port ${process.env.PORT || 8080}`,
       );
     });
   })
   .catch(() => {
-    console.log("An error occured when connecting to the database");
+    console.log('An error occured when connecting to the database');
     process.exit(1);
   });
