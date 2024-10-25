09b3ce4a75b449279a8f6cc53d35529c003e49e2
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-25 11:07:10 +0100

    setting up the fw

--- fe/src/services/axios.tsx
+++ fe/src/services/axios.tsx
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 const version = 'api/v1';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://stanki.onrender.com'
+    : 'http://localhost:8080';
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:8080/${version}`,
+  baseURL: `${baseUrl}/${version}`,
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
